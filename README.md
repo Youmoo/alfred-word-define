2c88c6ae34d2c7ded4c3acf804761ec7cbf52a37
youmoo <youmoolee@gmail.com>
2016-05-07 17:32:49 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+alfred-word-define
+------
+
+Look up a word using alfred and youdao.
+
+![](word-define.gif)
+
+[Download](Word Define.alfredworkflow?raw=true)
+
+### Usage
+
+- type `def` to define a word
+- select some text then press `option c` to define the text
+
+### Requirements
+
+-- latest node `brew install node`
---
+++ define-compiled.js
@@ -0,0 +1,30 @@
+#! /usr/local/bin/node
+
+
+const _ = require('pipeable');
+
+const data = process.argv[2];
+
+_(data)._pi(JSON.parse)._pi(json => [{ key: '音标', value: json.basic['us-phonetic'] }].concat(json.translation, json.basic.explains, json.web)).map((v, i, a) => {
+    return typeof v === 'string' ? {
+        title: v,
+        subtitle: ''
+    } : {
+        title: v.value,
+        subtitle: v.key
+    };
+}).map(v => v && `
+                <item>
+                    <title><![CDATA[${ v.title }]]></title>
+                    <subtitle><![CDATA[${ v.subtitle }]]></subtitle>
+                </item>
+            `)._pi(items => {
+    console.log(`
+                 <?xml version="1.0" encoding="UTF-8"?>
+            <items>
+                ${ items.join() }
+            </items>
+            `);
+});
+
+//# sourceMappingURL=define-compiled.js.map
